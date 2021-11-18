6188a9834426470aea91d63fe4c96bc3e1444d20
isabela1s <isabelasalvador@gmail.com>
2021-11-17 23:46:41 -0300

    tentativa de add js nas categorias e separaçao do css do quem somos

---
+++ public/js/javaScript.js
@@ -0,0 +1,16 @@
+/*
+
+
+    PAGINA DE CATEGORIAS
+
+
+        function alertausuario(texto){
+    
+        if(catego==""){
+        alert(texto);
+    }    
+    
+}
+function validaCampos(texto){
+    
+}*/
