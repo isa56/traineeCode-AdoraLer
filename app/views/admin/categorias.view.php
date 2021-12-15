4fe0d2b1ab88aa669aa659a1de33b68d3131aff8
isabela1s <isabelasalvador@gmail.com>
2021-12-15 17:36:20 -0300

    default arrumado

--- app/views/admin/categorias.view.php
+++ app/views/admin/categorias.view.php
@@ -3,8 +3,7 @@
 
 <head>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../../../public/css/styles.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../../../public/js/javaScript.js"></script>
@@ -13,16 +12,10 @@
 </head>
 
 <body>
-    
-    <?php include_once('app\views\includes\navbarAdm.php'); 
-   
+
+    <?php include_once('app\views\includes\navbarAdm.php');
+
     ?>
-    
-    <?php foreach ($categorias as $categoria) : ?>
-     <h2>
-    <?= $categoria->categoria ?>
-    </h2>
-    <?php endforeach; ?>
     <div class="pegatd">
         <div class="container">
             <H1 class="text-center">CATEGORIAS-Administração </H1><br>
@@ -33,8 +26,7 @@
                         <label>Nome da Categoria:</label>
                         <input type="text" name="txtCategoria" class="form-control" maxlength="50"><br>
                         <div class="d-grid gap-2">
-                            <button onclick="alertausuario('mensagem do java')" class="btn btn-primary"
-                                type="button">Cadastrar</button>
+                            <button onclick="alertausuario('mensagem do java')" class="btn btn-primary" type="button">Cadastrar</button>
                         </div>
 
                     </form>
@@ -42,50 +34,53 @@
                     <hr />
                     <h2 class="text-center">Categorias cadastradas</h2>
                     <div class="row">
-                    <div class="col-md-12">
-                        <div class="table-responsive">
-                            <table class="table" >
-                                <thead >
-                                <tr class="dif">
-                                    <th>Nome</th>                      
-                                    <th>Editar</th>
-                                    <th>Deletar</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach($categorias as $categoria) : ?>
-                                        
-                                    <tr> 
-                                        
-                                        <td class="td"><?= $categoria->categoria; ?></td>                    
-                                        <td>
-                                            <div style="display: flex;">
-                                                <form method = "GET" action = "editarc">
-                                                    <input type = "hidden" name = "id" value=<?= $categoria->id; ?>>
-                                                    <input type = "hidden" name = "categoria" value=1; ?>
-                                                    <button type="submit" class="btn"><i class="bi bi-pencil-square"></i>
-                                                </form>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div style="display: flex;">
-                                                <form method = "POST" action = "delete_categoria">
-                                                    <input type = "hidden" name="id" value=<?= $categoria->id; ?>>
-                                                    <input type = "hidden" name = "categoria" value=1; ?>
-                                                    <button  type="submit" class="btn"><i class="bi bi-trash"></i>
-                                                    
-                                                </form>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
+                        <div class="col-md-12">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr class="dif">
+                                            <th>Nome</th>
+                                            <th>Editar</th>
+                                            <th>Deletar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($categorias as $categoria) : ?>
+
+                                            <tr>
+
+                                                <td class="td"><?= $categoria->categoria; ?></td>
+                                                <td>
+                                                    <div style="display: flex;">
+                                                        <form method="GET" action="editarc">
+                                                            <?php if ($categoria->categoria != "default") : ?>
+                                                                <input type="hidden" name="id" value=<?= $categoria->id; ?>>
+                                                                <input type="hidden" name="categoria" value=1;>
+                                                                <button type="submit" class="btn"><i class="bi bi-pencil-square"></i>
+                                                            <?php endif; ?>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div style="display: flex;">
+                                                        <form method="POST" action="delete_categoria">
+                                                            <?php if ($categoria->categoria != "default") : ?>
+                                                                <input type="hidden" name="id" value=<?= $categoria->id; ?>>
+                                                                <input type="hidden" name="categoria" value=1; ?>
+                                                                <button type="submit" class="btn"><i class="bi bi-trash"></i>
+                                                            <?php endif; ?>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
 </body>
